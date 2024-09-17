5643de9b4f758ecf2e075501a1726648302f6ef2
Abner-Carnielle <125026806+Abner-Carnielle@users.noreply.github.com>
2024-09-17 11:29:42 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,6 @@
+function cadastro_cliente(){
+    const no_cl = document.getElementById("nome_cliente").value;
+    const en_cp = document.getElementById("endereco_cep").value;
+    document.getElementById("cliente_nome").innerText = no_cl;
+    document.getElementById("cep_endereco").innerText = en_cp;
+}
