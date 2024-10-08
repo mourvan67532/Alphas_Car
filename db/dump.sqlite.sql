5e205cf5af2a4c22e3c05b1a1aea1647895ce648
JoaoBaez2ads <133768864+JoaoBaez2ads@users.noreply.github.com>
2024-10-08 11:30:50 -0300

    Add files via upload

---
+++ db/dump.sqlite.sql
@@ -0,0 +1,16 @@
+-- TABLE
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE veiculo(
+id_veiculo integer Primary Key AUTOINCREMENT,
+marca varchar(20),
+modelo varchar(100),
+ano varchar(20),
+placa varchar(10)
+);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
