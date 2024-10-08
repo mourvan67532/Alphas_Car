1913e4293f9fc1b75bda83ca968ba88e46e905bf
Victor dos Santos <127848624+otaldovitin@users.noreply.github.com>
2024-10-08 11:29:22 -0300

    Add files via upload

---
+++ db/Cliente.sql
@@ -0,0 +1,18 @@
+-- TABLE
+CREATE TABLE cliente(
+  ID_cliente integer Primary key AUTOINCREMENT,
+  Nome varchar(100),
+  CPF integer unique not null,
+  Fone integer not null,
+  CEP varchar(20),
+  Bairro varchar(20),
+  Rua varchar(20),
+  Numero varchar(5)
+ );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
