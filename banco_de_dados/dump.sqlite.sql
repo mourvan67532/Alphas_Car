167941e434ed01e746fcd8a1495ddb437759764f
Luizz <126569871+Luizescola2212@users.noreply.github.com>
2024-10-08 11:34:20 -0300

    Add files via upload

--- banco_de_dados/dump.sqlite.sql
+++ banco_de_dados/dump.sqlite.sql
@@ -1,12 +1,9 @@
 -- TABLE
-CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
-CREATE TABLE veiculo(
-id_veiculo integer Primary Key AUTOINCREMENT,
-marca varchar(20),
-modelo varchar(100),
-ano varchar(20),
-placa varchar(10)
-);
+CREATE TABLE mecanico(
+	cpf INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome VARCHAR(100),
+    especializacoes TEXT NOT NULL
+  );
  
 -- INDEX
  
