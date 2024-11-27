d1734064d7b5117740bee60c533a659b2cfe977b
JoaoBaez2ads <133768864+JoaoBaez2ads@users.noreply.github.com>
2024-11-27 10:45:13 -0300

    Add files via upload

---
+++ consulta-cep.js
@@ -0,0 +1,13 @@
+//viacep.com.br/ws/RS/Porto Alegre/Domingos Jose/json/
+
+let rua = document.querySelector('#rua');
+let cidade = document.querySelector('#cidade');
+let uf = document.querySelector('#estado');
+let btnCep = document.querySelector('#btnCep');
+let listaCep = document.querySelector('#listaCep');
+
+rua.value = 'Domingos Jose';
+cidade.value = 'Porto Alegre';
+uf.value = 'RS';
+
+btnCep.addEventListener
