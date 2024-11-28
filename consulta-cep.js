a7cadec73637774bdbc2d51397ae965246cb9162
JoaoBaez2ads <133768864+JoaoBaez2ads@users.noreply.github.com>
2024-11-28 09:40:12 -0300

    Add files via upload

--- consulta-cep.js
+++ consulta-cep.js
@@ -3,11 +3,33 @@
 let rua = document.querySelector('#rua');
 let cidade = document.querySelector('#cidade');
 let uf = document.querySelector('#estado');
-let btnCep = document.querySelector('#btnCep');
+let btnCep = document.querySelector('#btnBuscarCep');
 let listaCep = document.querySelector('#listaCep');
 
 rua.value = 'Domingos Jose';
 cidade.value = 'Porto Alegre';
 uf.value = 'RS';
 
-btnCep.addEventListener
+btnCep.addEventListener('click', function(e) {
+    e.preventDefault();
+    
+    let urlBase = 'https://viacep.com.br/ws/';
+    let parametros = uf.value + '/' + rua.value + '/json/';
+    let callback = '?callback=popularNaoSeiMeuCep';
+
+    let script = document.createElement('script');
+    script.src = urlBase + parametros + callback;
+    document.body.appendChild(script);
+});
+
+function popularNaoSeiMeuCep(resposta) {
+
+    if(!Array.isArray(resposta)) {
+        alert('O retorno não é uma lista de CEPs');
+        return;
+    }
+
+    resposta.forEach(function(i)) {
+        
+    }
+}
